534efe648d5c5521a3fb7869c195c4dcf9d56fda
Sarvesh Tiwari <sarvesht.aane@gmail.com>
2023-12-26 14:38:46 +0400

    updating index for 2 commits

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 
-/* file adding to test cherry-pick */ 
-
+/* file adding to test cherry-pick 1 commit */ 
+/* file adding to test cherry-pick 2 commits */
 phpinfo();
 
-/* file adding to test cherry-pick */
+/* file adding to test cherry-pick 1 commit */
+/* file adding to test cherry-pick 2 commits */
